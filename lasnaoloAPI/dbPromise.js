b2a6d8af065d3f01b3132aadc20a6ecc74cd1702
Karjalainen Jetro <s1700583@student.businesscollege.fi>
2019-03-08 08:50:26 +0200

    API yhteys tietokantaan

---
+++ lasnaoloAPI/dbPromise.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql');
+
+class Database {
+    constructor(config) {
+        this.connection = mysql.createPool(config);
+    }
+
+    async query(sql, values) {
+        return new Promise((resolve, reject)=> {
+            this.connection.query(sql, values, (error, results) => {
+                if (error) {
+                    reject (error);
+                }
+                console.log(results);
+                resolve(results);
+                
+            });
+        });
+    }
+
+    async close() {
+        return new Promise((resolve, reject)=> {
+            this.connection.end(error => {
+                if (error) {
+                    reject (error);
+                };
+                resolve();
+            })
+        });
+    }
+}
+
+module.exports = Database;
--- lasnaoloAPI/routes/areas.js
+++ lasnaoloAPI/routes/areas.js
@@ -1,12 +1,69 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/:area/presence', (req, res) => {
+let dbConfig = require("../config.json");
+let Database = require("../dbPromise.js");
+let database = new Database(dbConfig);
+
+router.post('/:area/presence', async (req, res) => {
     console.log('Area: ' + req.params.area);
-    console.log('Content:' + req.body);
+    console.log('Content:' + req.body.tags);
 
     if(!req.body.tags) {
         res.status(400).send({error: 'Mandatory attribute tags missing'});
+        return;
+    }
+    
+    
+    let results = await database.query(
+        "SELECT id FROM area WHERE code = ? ",
+        [req.params.area]
+    );
+    
+    let areaId;
+
+    if(results && results.length > 0) {
+        areaId = results[0].id;
+    }
+    else {
+        res.status(400).send({error: "Invalid area"});
+        return;
+    }
+
+   
+    //console.log(areaId);
+
+    let personId = [];
+    console.log(req.body.tags);
+    for(let i = 0; i < req.body.tags.length; i++) {
+        try {
+            results = await database.query(
+                "SELECT id FROM person WHERE tagId = ?",
+                [req.body.tags[i]]
+            );
+            if(results && results.length > 0) {
+                personId.push(results[0].id);
+            } else {
+                console.log("aaaaaa")
+            }
+        }
+        catch(error) {
+            console.log("error: " + error);
+        }
+    }
+    
+    console.log(personId);
+
+    for(let i = 0; i < personId.length; i++) {
+        try {
+            results = await database.query(
+                "INSERT INTO presence (person, area) VALUES (?, ?)",
+                [personId[i], areaId]
+            ); 
+        } catch (error) {
+            console.log("Error: " + error);
+        }
+        
     }
 
     //tarkista pyyntö
